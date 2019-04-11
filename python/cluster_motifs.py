01d384137e68054cf3f7044cec6c319c525a1348
Takaho A. Endo <takaho@dhcp-161-175.rcai.riken.jp>
2019-04-11 18:50:28 +0900

    non-redundant motif outputs

--- python/cluster_motifs.py
+++ python/cluster_motifs.py
@@ -17,18 +17,23 @@ parser.add_argument('-g', default='out.pdf')
 args = parser.parse_args()
 
 motifs = pd.read_csv(args.i, sep='\t', index_col=0)
-tfs = motifs.columns[1:]
-matrix = motifs[tfs].copy()
+celltypes = motifs.columns[1:]
+matrix = motifs[celltypes].copy()
 matrix[matrix < 1] = 1
+cmat = matrix.copy()
+cmat[cmat <= 1] = 0
+cmat[cmat > 2] = 1
 n_clusters = args.n
 clu = sklearn.cluster.AgglomerativeClustering(n_clusters=n_clusters)
-clu.fit_predict(np.log(matrix))
+# clu.fit_predict(np.log2(matrix))
+clu.fit_predict(cmat)#np.log2(matrix))
 
-clorder = [[] for i in range(n_clusters)]
+# clorder = [[] for i in range(n_clusters)]
 values = matrix.values
-ethr = 2
+ethr = 1
 cluster_score = [[] for i in range(n_clusters)]
 n_cols = matrix.shape[1]
+smat = []
 for cl in range(n_clusters):
     scores = [0] * n_cols
     n = 0
@@ -39,20 +44,35 @@ for cl in range(n_clusters):
                 scores[j] += 1
         n += 1
     amx = np.argmax(scores)
-    if n == 0: n += 1
-    if scores[amx] < n / 4:
-        score = 10
-    else:
-        score = amx
-    clorder[cl] = score
+    # print(cl, scores, amx, n)
+    if n == 0: n = 1
+    smat.append([cl, ] + [x_ / n for x_ in scores])
+index = 0
+def __gen_key(a):
+    n = len(a) - 1
+    value = 0
+    for i in range(n):
+        value *= 100
+        value += min(10, a[i + 1])
+        # if a[i] != b[i]:
+        #     return -(a[i] < b[i])
+    return value
+# clorder = [0] * len(smat)
+# print(smat)
+# for i, l in enumerate(sorted(smat, key=__gen_key)):
+#     clorder[l[0]] = i
+#     print(__gen_key(l))
 
+# print(clorder)
+clorder = [0, 1, 2, 3, 4]
+# clorder = 
 motif_names= motifs.index
 tf_names = matrix.columns
 ost = sys.stdout
 if args.o is not None:
     ost = open(args.o, 'w')
-ost.write('Motif\tCluster\tSequence\t' + '\t'.join(tfs) + '\n')
-
+ost.write('Motif\tCluster\tSequence\t' + '\t'.join(celltypes) + '\n')
+print(ost)
 if args.g:
     import reportlab.pdfgen.canvas
     import reportlab.lib.colors
@@ -72,13 +92,20 @@ if args.g:
         reportlab.lib.colors.Color(.3, .6, .1, .5), # GC1
     ]
     gray = reportlab.lib.colors.Color(0,0,0,.4)
-    for i, tf in enumerate(tfs):
+    for i, celltype in enumerate(celltypes):
         cnv.setFillColor('black')
-        cnv.drawString(left + xstep * (i + .5) - cnv.stringWidth(tf) * .5, top + 15, tf)
+        cnv.drawString(left + xstep * (i + .5) - cnv.stringWidth(celltype) * .5, top + 15, celltype)
 else:
     cnv = None
 
 y = top
+
+def enrichment2size(enr, maxval=8):
+    if enr <= 1.0:
+        return 0.1
+    else:
+        return (1.0 + np.log2(min(maxval, enr))) * .1
+
 for cl in sorted(range(n_clusters), key=lambda v:clorder[v]):
     for i, label in enumerate(clu.labels_):
         if label != cl:
@@ -88,7 +115,7 @@ for cl in sorted(range(n_clusters), key=lambda v:clorder[v]):
         if cnv:
             cnv.setFillColor('black')
             cnv.drawString(left - 70, y, name)
-            cnv.drawString(left + xstep * (len(tfs) + .5), y, motifs['Motif'][name])
+            cnv.drawString(left + xstep * (len(celltypes) + .5), y, motifs['Motif'][name])
         vmax = 0
         for j, enrichment in enumerate(matrix.loc[name].values):
             if enrichment <= 1.0:
@@ -99,14 +126,15 @@ for cl in sorted(range(n_clusters), key=lambda v:clorder[v]):
             if cnv:
                 cx = left + xstep * (j + .5)
                 cy = y + ystep * .5
-                xsize = (.2 + min(5, np.log2(enrichment))) * xstep * .15
-                ysize = xsize / xstep * ystep * 2
+                xsize = enrichment2size(enrichment) * xstep#(1.0 + min(2, np.log2(enrichment))) * xstep * .15
                 if enrichment <= 1.0:
+                    xsize = 1.0
                     cnv.setFillColor(gray)
                     roundlevel = 0.5
                 else:
                     cnv.setFillColor(colors[j])
                     roundlevel = xsize / 2
+                ysize = xsize / xstep * ystep * 2
                 cnv.roundRect(cx - xsize, cy - ysize, xsize * 2, ysize * 2, roundlevel, 0, fill=1)
                     #cnv.rect(cx + xstep * j - xsize, cy - ysize, xsize * 2, ysize * 2, 0, fill=1)
         y -= ystep
@@ -114,8 +142,12 @@ for cl in sorted(range(n_clusters), key=lambda v:clorder[v]):
 if args.o: ost.close()
 if cnv:
     for i in range(0, 5):
-        logenr = i * .8 + 1
-        xsize = (.2 + min(5, logenr)) * xstep * .15
+        if i == 0:
+            xsize = 1.0
+        else:
+            xsize = enrichment2size(2 ** (i - 1)) * xstep
+        # logenr = i + 1
+        # xsize = enrichment2size( 2 ** logenr) * xstep
         ysize = xsize / xstep * ystep * 2
         roundlevel = xsize / 2
         cnv.setFillColor('navy')
--- python/enumerate_reproduced_motifs.py
+++ python/enumerate_reproduced_motifs.py
@@ -4,215 +4,9 @@ Aggregate peaks detected using HOMER and MEME (DREME)
 import os, sys, re
 import pathlib, argparse
 import numpy as np
+# import pandas as pd
 import collections
-
-class MEMEMotif(object):
-    def __init__(self, name, length, n_sites=0, score=0, matrix=None):
-        self.name = name
-        self.length = length
-        self.n_sites = n_sites
-        self.score = score
-        self.set_matrix(matrix)
-        self.background = [.25] * 4
-        # self.matrix = matrix
-    def set_background(self, bg):
-        if len(bg) == 4:
-            self.background = bg
-    def set_matrix(self, mat):
-        if mat is None:
-            return
-        if len(mat) != self.length:
-            raise Exception('matrix shape should be [{},4]'.format(self.length))
-        for r in mat:
-            if len(r) != 4:
-                raise Exception('matrix shape should be [{},4]'.format(self.length))
-        self.matrix = mat
-    def __repr__(self):
-        ostr = '{} Score:{:.2e} Sites:{}\n'.format(self.name, self.score, self.n_sites)
-        for i in range(4):
-            line = ''
-            for j in range(self.length):
-                if j > 0: line += ' '
-                line += '{:.3f}'.format(self.matrix[j][i])
-            ostr += line + '\n'
-        return ostr
-    def output_meme(self, ost):
-        a, c, g, t = self.background
-        ost.write('MEME version 5\n\nALPHABET= ACGT\n\nstrands: + -\n\nBackground letter frequencies (from uniform background):\nA {:.4f} C {:.4f} G {:.4f} T {:.4f}\n\n'.format(a, c, g, t))
-        ost.write('MOTIF {}\n\nletter-probability matrix: alength= 4 w= {} nsites= {} E={:.3e}\n\n'.format(self.name, self.length, self.n_sites, self.score))
-        for i in range(4):
-            line = ''
-            for j in range(self.length):
-                if j > 0: line += ' '
-                line += '{:.3f}'.format(self.matrix[j][i])
-            ost.write(line)
-            ost.write('\n')
-    @classmethod
-    def load_dreme(cls, fn):
-        if os.path.isdir(fn):
-            title = os.path.basename(fn)
-            fn = os.path.join(fn, 'dreme.txt')
-        elif os.path.isfile(fn):
-            title = os.path.basename(os.path.dirname(fn))
-        else:
-            raise Exception('Skip {} : directory or dreme.txt is acceptable\n'.format(fn))
-
-        if os.path.exists(fn) is False:
-            raise Exception('no {}\n'.format(fn))
-
-        with open(fn) as fi:
-            contents = fi.readlines()
-        num_lines = len(contents)
-        i = 0
-        motifs = []
-        background = [.25, .25, .25, .25]
-        while i < num_lines:
-            line = contents[i]
-            if line.startswith('Background letter frequencies'):
-                m = re.match('A\\s+([\\d\\.]+)\\s+C\\s+([\\d\\.]+)\\s+G\\s+([\\d\\.]+)\\s+T\\s+([\\d\\.]+)\\s+', contents[i+1])
-                if m:
-                    acgt = [float(x_) for x_ in m.groups()]
-                    if len(acgt) == 4:
-                        background = [float(x_) / sum(acgt) for x_ in acgt]
-                i += 1
-            elif line.startswith('MOTIF'):
-                m = re.match('MOTIF\\s+(\\w+)', line)
-                name = m.group(1)
-                start = -1
-                j = i + 1
-                while j < num_lines:
-                    if contents[j].startswith('letter-probability'):
-                        start = j
-                        break
-                    j += 1
-                if start < 0:
-                    i += 1
-                    continue
-                info = contents[start]
-                m = re.search('alength=\\s*(\\d+) w=\\s*(\\d+) nsites=\\s*(\\d+) E=\\s*([\\d\\.e\\-]+)', info) #7.2e-543', info)
-                l = int(m.group(1))
-                w = int(m.group(2))
-                n = int(m.group(3))
-                score = float(m.group(4))
-                j = start + 1
-                matrix = []
-                while j < min(start + 1 + w, num_lines):
-                    freq = [float(x_) for x_ in re.split('\\s+', contents[j].strip())]
-                    matrix.append(freq)
-                    j += 1
-                motif = MEMEMotif(name, w, n, score, matrix)
-                # motif.output_meme(sys.stdout)
-                motifs.append(motif)
-                i = j + 1
-            else:
-                i += 1
-        return motifs
-
-class HomerMotif(object):
-    def __init__(self, motif, name, matrix, info):
-        self.motif = motif
-        self.name = name
-        self.dbname = None
-        self.matrix = matrix
-        self.info = info
-    def set_knwon_name(self, name):
-        if self.dbname is None:
-            self.dbname = []
-        self.dbname.append(name)
-
-    def __get_enrichment(self):
-        if 'T' in self.info and 'B' in self.info:
-            t = self.info['T'][1]
-            b = self.info['B'][1]
-            if t > 0 and b > 0:
-                return t / b
-        return 1.0
-
-    def __get_hits(self):
-        if 'T' in self.info:
-            return self.info['T'][0]
-        else:
-            return 0
-
-    def __get_background(self):
-        if 'B' in self.info:
-            return self.info['B'][0]
-        else:
-            return 0
-
-    def __get_known_name(self):
-        priority_names = 'SEP1', 'SOC1', 'PI', 'AGL15', 'ATHB-5', 'SEP3', 'TBP3', 'SEP4', 'SEP5',  
-        if self.dbname is None:
-            return '.'
-        for n in priority_names:
-            if n in self.dbname:
-                return n
-        return self.dbname[0]
-
-    enrichment = property(__get_enrichment)
-    hits = property(__get_hits)
-    background = property(__get_background)
-    known = property(__get_known_name)
-
-    def __repr__(self):
-        ostr = '{} {}\n'.format(self.motif, self.name)
-        for i in range(4):
-            line = ''
-            for j in range(self.length):
-                if j > 0: line += ' '
-                line += '{:.3f}'.format(self.matrix[j][i])
-            ostr += line + '\n'
-        return ostr
-    @classmethod
-    def convert_to_meme(cls, motifs, filename_template=None):
-        if filename_template is None:
-            filename_template = os.path.join(os.path.dirname(__file__), 'meme_template.txt')
-        with open(filename_template) as fi:
-            contents = fi.read()
-        ostr = contents
-        for i, motif in enumerate(motifs):
-            ostr += 'MOTIF {} HOMER-{}\n\n'.format(motif.motif, i + 1)#motif.name)
-            ostr += 'letter-probability matrix: alength= 4 w= {} nsites= {} E= {:.4e}\n'.format(motif.length, motif.info.get('T', [0,0])[0], motif.info.get('P', 0))
-            for row in motif.matrix:
-                ostr += '{:.6f}\t{:.6f}\t{:.6f}\t{:.6f}\n'.format(row[0], row[1], row[2], row[3])
-            ostr += '\n\n'
-        return ostr
-
-    length = property(lambda s:len(s.matrix))
-    @classmethod
-    def load_motifs(cls, srcdir):
-        motifs = {}
-        for fn in os.listdir(srcdir):
-            m = re.match('motif(\\d+)\\.motif$', fn)
-            if m is None:
-                continue
-            number = int(m.group(1))
-            with open(os.path.join(srcdir, fn)) as fi:
-                # items = re.split('\\s+', fi.readline()[1:-1])
-                items = fi.readline()[1:-1].split('\t')
-                # print(items)
-                motif = items[0]
-                name = items[1]
-                evalue = float(items[2])
-                score = float(items[3])
-                dig = float(items[4])
-                info = {}
-                for elem in items[5].split(','):
-                    key, val = elem.split(':', 1)
-                    if key == 'T' or key == 'B':
-                        m = re.match('([\\d\\.]+)\\(([\\d\\.]+)%\\)', val)
-                        if m:
-                            count, percentage = float(m.group(1)), float(m.group(2))
-                            info[key] = count, percentage
-                    elif key == 'P':
-                        pvalue = float(val)
-                info['E'] = evalue
-                matrix = []
-                for line in fi:
-                    items = re.split('\\s+', line.strip())
-                    matrix.append([float(x) for x in items])
-                motifs[number] = HomerMotif(motif, name, matrix, info)
-        return [motifs[n] for n in sorted(motifs.keys())]
+from motifdetection import MEMEMotif, HOMERMotif, ATACMotif, load_consensus
 
 def check_match(seq1, seq2):
     import Bio.Seq
@@ -226,12 +20,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-t', nargs='+', default=['AtML1', 'SPCH', 'MUTE', 'FAMA', 'GC1'])
 parser.add_argument('-o', default='out')
 parser.add_argument('-i', help='source directory')
+parser.add_argument('-n', type=int, default=10)
+parser.add_argument('--motif', default=None)
 parser.add_argument('--minimum-hits', type=int, default=100, help='minimum target sites')
 
 args = parser.parse_args()
+filename_output = args.o
 minimum_hits = args.minimum_hits
 reproduced_motifs = {}
+hit_threshold = args.n
 
+if args.motif is not None:
+    motif_db = load_consensus(args.motif)
+else:
+    motif_db = {}
+for key, val in motif_db.items():
+    print('{}\t{}'.format(key, val))
+exit()    
 # motifs = MEMEMotif.load_dreme('500/GC1.dreme')
 # seq = 'TGGGCC'
 # for key in motifs:
@@ -252,80 +57,147 @@ def tomtom(filename_input, filename_output, db, params={}):
     sys.stderr.write(' '.join(cmd) + '\n')
     subprocess.Popen(cmd).wait()
 
-for tf in args.t:
-    sys.stderr.write('#{}\n'.format(tf))
-    src_dreme = os.path.join(srcdir, tf + '.dreme/dreme.txt')
-    src_tomtom = os.path.join(srcdir, tf + '.dreme/tomtom/tomtom.txt')
-    # src_homer = os.path.join(srcdir, tf + '.homer/homerMotifs.all.motifs')
-    srcdir_homer = os.path.join(srcdir, tf + '.homer/homerResults/')#.all.motifs')
-    src_homer_tomtom = os.path.join(srcdir, tf + '.homer/tomtom/tomtom.txt')
-    homer_tomtom_input = os.path.join(srcdir, tf + '.homer/tomtom.input')
+celltypes = args.t
+
+for celltype in celltypes:
+    sys.stderr.write('#{}\n'.format(celltype))
+    src_dreme = os.path.join(srcdir, celltype + '.dreme/dreme.txt')
+    src_tomtom = os.path.join(srcdir, celltype + '.dreme/tomtom/tomtom.txt')
+    # src_homer = os.path.join(srcdir, celltype + '.homer/homerMotifs.all.motifs')
+    srcdir_homer = os.path.join(srcdir, celltype + '.homer/homerResults/')#.all.motifs')
+    src_homer_tomtom = os.path.join(srcdir, celltype + '.homer/tomtom/tomtom.txt')
+    homer_tomtom_input = os.path.join(srcdir, celltype + '.homer/tomtom.input')
     tomtom = collections.OrderedDict()
     homer = collections.OrderedDict()
     dreme = collections.OrderedDict()
-    for mot in MEMEMotif.load_dreme(src_dreme):
+
+    # DREME
+    dreme_motifs = MEMEMotif.load_motifs(src_dreme)
+    ATACMotif.annotate_motifs(dreme_motifs, src_tomtom)
+    for mot in dreme_motifs:
         dreme[mot.name] = mot
 
     #HOMER
     motif_stat = {}
-    homer_motifs = HomerMotif.load_motifs(srcdir_homer)
-    if os.path.exists(src_homer_tomtom) is False:
+    homer_motifs = HOMERMotif.load_motifs(srcdir_homer)
+    if os.path.exists(src_homer_tomtom) is False: # execute if no tomtom outputs found
         if os.path.exists(homer_tomtom_input) is False or os.path.getsize(homer_tomtom_input) < 1000:
             with open(homer_tomtom_input, 'w') as fo:
-                fo.write(HomerMotif.convert_to_meme(homer_motifs))
-        sys.stderr.write('{} is not prepared tomtom, execute tomtom with {}\n'.format(tf, homer_tomtom_input))
+                fo.write(HOMERMotif.convert_to_meme(homer_motifs))
+        sys.stderr.write('{} is not prepared tomtom, execute tomtom with {}\n'.format(celltype, homer_tomtom_input))
         tomtom(homer_tomtom_input, src_homer_tomtom, db)
-    else:
-        with open(src_homer_tomtom) as fi:
-            for line in fi:
-                items = line.strip().split('\t')
-                if line.startswith('#') or len(items) < 5: continue
-                # print(items)
-                seq = items[0]
-                dbname = items[1]
-                for m in homer_motifs:
-                    if m.motif == seq or m.name == seq or m.name == dbname or m.motif == dbname:
-                        m.set_knwon_name(dbname)
-                        if dbname not in homer:homer[dbname] = []
-                        homer[dbname].append(seq)
-                        # print('{} {} //  {} {}'.format(seq, dbname, m.motif, m.name))
-                        break
-    columns.append(tf)
-    #DREME-TOMTOM
-    if os.path.exists(src_tomtom) is False or os.path.getsize(src_tomtom) < 1000:
-        tomtom(src_dreme, src_tomtom, db)
-    with open(src_tomtom) as fi:
-        for line in fi:
-            items = line.strip().split('\t')
-            if line.startswith('#') or len(items) < 5: continue
-            name = items[0]
-            dbname = items[1]
-            if dbname not in tomtom: tomtom[dbname] = []
-            tomtom[dbname].append(name)
-            # tomtom[name].append(dbname)
+    ATACMotif.annotate_motifs(homer_motifs, src_homer_tomtom)#tomtom)
 
-    # print(len(tomtom), len(homer))
-    results[tf] = {}
-    for i, motif in enumerate(homer_motifs):
-        if motif.hits < minimum_hits:
-            continue
-        # search database name
-        touched = set()
+    # Examine reproducibility
+    shared_names = set()
+    reproduced_motifs = []
+    print('MEME {}, HOMER {}'.format(len(dreme_motifs), len(homer_motifs)))
+    # for m in homer_motifs:
+    #     print(m.get_known())
+    # print()
+    # for m in dreme_motifs:
+    #     print(m.get_known())
+    # exit()
+    for motif in homer_motifs:
         reproduced = False
-        # print(motif.motif)
-        for dbname, seqs in homer.items():
-            for seq in seqs:
-                # print(seq, motif.motif)
-                if motif.motif == seq:
-                    # print("NAMED")#motif.motif, seq)
-                    if dbname in tomtom: # reproduced
-                        reproduced = True
-                        break
+        for m in dreme_motifs:
+            # print(motif.consensus, m.consensus, m.get_known())
+            if motif.consensus in m.consensus or m.consensus in motif.consensus:
+                reproduced = True
+            for c in motif.get_shared_consensus(m):
+                shared_names.add(c)
+                reproduced = True
         if reproduced:
-            name = motif.known
-            rows.add(name)
-            print('{}\t{}\t{:.3f}'.format(i + 1, name, motif.enrichment))#str(motif.dbname), motif.name))
-            results[tf][motif.known] = motif
+            reproduced_motifs.append(motif)
+    arranged = ATACMotif.classify_and_select_best(reproduced_motifs)
+    # print(len(arranged))
+    results[celltype] = arranged
+    for name, m in arranged.items():
+        print('{}\t{}\t{:.2f}\t{:.0f}\t{}'.format(celltype, m.consensus, m.enrichment, m.n_sites, name))
+
+normalized_names = {}
+enrichment_matrix = {}
+all_motifs = set()
+for celltype, motifs in results.items():
+    results = {}
+    used_motif = set()
+    for joined_name, motif in motifs.items():
+        names = joined_name.split('//')
+        joined = None
+        for name in names:
+            if name in results:
+                if results[name].n_sites < motif.n_sites:
+                    results[name] = motif
+            else:
+                results[name] = motif
+                all_motifs.add(name)
+    enrichment_matrix[celltype] = results
+
+def name_with_priority(name):
+    if name in ('SEP1', 'SOC1', 'PI', 'AGL15', 'ATHB-5', 'SEP3', 'TBP3', 'SEP4', 'SEP5',  'TCP15'):
+        return ' ' + name
+    return name
+
+used_ids = set()
+unique_names = []
+for m in sorted(all_motifs, key=name_with_priority):
+    ids = []
+    novel = False
+    for enr in enrichment_matrix.values():
+        if m in enr:
+            idnum = id(enr[m])
+            if idnum not in used_ids:
+                novel = True
+                used_ids.add(idnum)
+            ids.append(idnum)
+    if novel:
+        unique_names.append(m)
+
+#valuematrix = np.zeros((len(unique_names), len(celltypes)))
+#valuematrix = []
+with open(filename_output, 'w') as fo:
+    header = 'NAME\tMotif\t' + '\t'.join(celltypes) + '\n'
+    fo.write(header)
+    for name in unique_names:
+        row = '{}\t{}'.format(name, motif_db.get(name, name))
+        for celltype in celltypes:
+            enr = enrichment_matrix[celltype]
+            if name in enr and enr[name].n_sites > hit_threshold:
+                value = enr[name].enrichment
+                visible = True
+            else:
+                value = 1.0
+            row += '\t{:.6f}'.format(value)
+        if visible:
+            fo.write(row + '\n')#\t{:.6f}'.format(value))
+
+#df.to_csv(filename_output, sep='\t')
+exit()
+#             if name in normalized_names:
+#                 joined = normalized_names[name]
+#                 for n in names:
+#                     joined.add(n)
+#                 for n in names:
+#                     normalized_names[n] = joined
+#                 break
+#         if joined is None:
+#             joined = set(names)
+#             for n in names:
+#                 normalized_names[n] = joined
+# touched = set()
+# for n, alias in normalized_names.items():
+#     if id(alias) in touched:
+#         continue
+#     print(','.join(sorted(alias)))
+#     touched.add(id(alias))
+# print(len(touched))
+# exit()
+                
+
+
+
+
+# exit()
 
 # output results
 indexes = list(sorted(rows))
@@ -345,7 +217,7 @@ with open(filename_output, 'w') as fo:
                 if seq is None: seq = detected.motif
                 values.append(detected.enrichment)
             else:
-                values.append(0)
+                values.append(1)
         if seq is not None:
             ostr = '{}\t{}'.format(name, seq)
             for v in values: ostr += '\t{:.4f}'.format(v)
